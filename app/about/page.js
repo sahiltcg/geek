9cda1f9108b413bc1051b363c66d824276c65975
sahiltcg <sahilahmedtcg@gmail.com>
2025-01-11 19:38:29 +0530

    This is a NextJS app for learning

---
+++ app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className='text-center text-orange-500 font-bold text-2xl my-10'>About Page</h1>
+      <p className='text-center text-sky-400 text-2xl font-bold '>Welcome to the About page of this app!</p>
+    </div>
+  )
+}
+
+export default page
